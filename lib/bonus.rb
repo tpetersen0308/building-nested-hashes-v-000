f9616bd0c4709440e15ecc638c9e6abb512578e1
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 19:37:11 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def bonus
   epic_tragedy = {
    :montague => {
@@ -22,6 +24,7 @@ def bonus
 
   #code your solution here:
   epic_tragedy[:montague][:hero][:status] = "dead"
+
   epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
